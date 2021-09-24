e4306a28a558af151b810fe5164aba577c218f3a
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-09-24 11:29:26 +0530

    Create Print all the Ancestors of a given Key.java

---
+++ Print all the Ancestors of a given Key.java
@@ -0,0 +1,14 @@
+boolean printAncestors(Node node, int target)
+	{
+		if (node == null)
+			return false;
+		if (node.data == target)
+			return true;
+		if (printAncestors(node.left, target)
+				|| printAncestors(node.right, target))
+		{
+			System.out.print(node.data + " ");
+			return true;
+		}
+		return false;
+	}
